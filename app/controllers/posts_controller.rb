a782f08cdcd01df4c9c4213ff7ab40568ddf6dea
Dhamzee Sherpa <dhamtsik@gmail.com>
2015-07-06 16:46:19 -0400

    blog created

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,50 @@
+class PostsController < ApplicationController
+	before_action :authenticate_admin!, except: [:index, :show]
+	def index
+		@posts = Post.all.order('created_at DESC')
+		@post = Post.new
+	end
+
+	def new
+		@post = Post.new
+	end 
+
+	def create
+		@post = Post.new(post_params)
+		if @post.save
+			redirect_to blog_path, notice: "Your post has been created!"
+
+		else 
+			render :new
+		end 
+	end
+
+	def show
+		set_param
+		
+	end 
+
+
+	def edit
+	end 
+
+	def update
+	end 
+
+	def destroy
+		@post.destroy
+		redirect_to root_path, alert: "your post was deleted"
+
+	end 
+
+	private
+
+	def post_params
+		params.require(:post).permit(:post, :title)
+	end 
+
+	def set_param
+		@post = Post.find(params[:id])
+	end 
+
+end
